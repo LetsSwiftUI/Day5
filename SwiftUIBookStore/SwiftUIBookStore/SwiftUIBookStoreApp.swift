9156785aa4183b8d279315e8528176ebd13eec54
Artsyork <cisspmit@naver.com>
2024-07-05 19:27:24 +0900

    init files

---
+++ SwiftUIBookStore/SwiftUIBookStore/SwiftUIBookStoreApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUIBookStoreApp.swift
+//  SwiftUIBookStore
+//
+//  Created by DY on 7/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftUIBookStoreApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
