d749fcd356f661dcf0ee2f54315de2584e1dd9ea
aroob6 <aroxb6@gmail.com>
2024-07-07 15:22:07 +0900

    viewStore 제거

--- SwiftUIBookStore/SwiftUIBookStore/View/Detail/BookDetailView.swift
+++ SwiftUIBookStore/SwiftUIBookStore/View/Detail/BookDetailView.swift
@@ -10,68 +10,75 @@ import ComposableArchitecture
 
 struct BookDetailView: View {
     var store: StoreOf<BookDetailFeature>
+//    @ObservedObject var viewStore: ViewStoreOf<BookDetailFeature>
+//    
+//    init(store: StoreOf<BookDetailFeature>) {
+//        self.store = store
+//        self.viewStore = ViewStore(self.store, observe: { $0 })
+//    }
     
     var body: some View {
         // WithViewStore는 Store를 ViewStore로 변경
         // Q) Store vs ViewStore?
         // @dynamicMemberLookup Store<State, Action>
         // @dynamicMemberLookup ViewStore<ViewState, ViewAction>: ObservableObject
-        WithViewStore(self.store, observe: { $0 }) { viewStore in
-            GeometryReader { geometry in
-                ScrollView {
-                    VStack(alignment: .center, spacing: 20) {
-                        Text(viewStore.bookDetail.title ?? "책 정보 없음")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .multilineTextAlignment(.center)
-                            .padding(.horizontal)
-                        
-                        if let imageString = viewStore.bookDetail.image,
-                           let imageUrl = URL(string: imageString) {
-                            let imageHeight = (geometry.size.height - geometry.safeAreaInsets.top - geometry.safeAreaInsets.bottom) / 2.5
-                            AsyncImageView(url: imageUrl)
-                                .frame(width: geometry.size.width - 100*2, height: imageHeight)
-                                .frame(height: 200)
-                        }
-                        
-                        HStack(spacing: 10) {
-                            Text(viewStore.bookDetail.rating ?? "-")
-                            Image(systemName: "star.fill")
-                                .foregroundColor(.yellow)
-                            Text(viewStore.bookDetail.price ?? "-")
-                                .fontWeight(.bold)
-                        }
-                        
-                        VStack {
-                            ForEach(viewStore.bookInfoList!) { item in
-                                BookDetailInnerView(title: item.category, content: item.content ?? "정보 없음")
-                                Spacer().frame(height: 10)
-                            }
-                        }
-                        
-                        Button(action: {
-                            
-                        }) {
-                            Text("Free eBook")
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .background(Color.blue)
-                                .foregroundColor(.white)
-                                .cornerRadius(10)
-                        }
+        
+        //WithViewStore(self.store, observe: { $0 }) { viewStore in
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .center, spacing: 20) {
+                    Text(store.bookDetail.title ?? "책 정보 없음")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
                         .padding(.horizontal)
-                        
-                        Text(viewStore.bookDetail.desc ?? "-")
-                            .padding(.horizontal)
+                    
+                    if let imageString = store.bookDetail.image,
+                       let imageUrl = URL(string: imageString) {
+                        let imageHeight = (geometry.size.height - geometry.safeAreaInsets.top - geometry.safeAreaInsets.bottom) / 2.5
+                        AsyncImageView(url: imageUrl)
+                            .frame(width: geometry.size.width - 100*2, height: imageHeight)
+                            .frame(height: 200)
                     }
-                    .padding()
+                    
+                    HStack(spacing: 10) {
+                        Text(store.bookDetail.rating ?? "-")
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                        Text(store.bookDetail.price ?? "-")
+                            .fontWeight(.bold)
+                    }
+                    
+                    VStack {
+                        ForEach(store.bookInfoList) { item in
+                            BookDetailInnerView(title: item.category, content: item.content ?? "정보 없음")
+                            Spacer().frame(height: 10)
+                        }
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Free eBook")
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding(.horizontal)
+                    
+                    Text(store.bookDetail.desc ?? "-")
+                        .padding(.horizontal)
                 }
-            }
-            .navigationBarHidden(true)
-            .onAppear {
-                viewStore.send(.fetchDetails(BookDetail_API.Request(isbn13: "9781617294136")))
+                .padding()
             }
         }
+        .navigationBarHidden(true)
+        .onAppear {
+            store.send(.fetchDetails(BookDetail_API.Request(isbn13: "9781617294136")))
+        }
+        //}
         
     }
 }
