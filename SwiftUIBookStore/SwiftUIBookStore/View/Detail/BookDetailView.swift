ce03bb3ec90206aae4f61f81fc958386f2ecc228
Chaewon Yoon <ycw0363@gmail.com>
2024-07-13 18:50:08 +0900

    API 연동

--- SwiftUIBookStore/SwiftUIBookStore/View/Detail/BookDetailView.swift
+++ SwiftUIBookStore/SwiftUIBookStore/View/Detail/BookDetailView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 import ComposableArchitecture
 
 struct BookDetailView: View {
-    var store: StoreOf<BookDetailFeature>
+    var store: StoreOf<BookDetailFeature> //TCA Strore 사용하여 상태, 동작 관리
     
     var body: some View {
         WithViewStore(self.store, observe: { $0 }) { viewStore in
+            //Store 상태 구독, ViewStore -> 상태 및 액션
             GeometryReader { geometry in
                 ScrollView {
                     VStack(alignment: .center, spacing: 20) {
@@ -38,7 +39,8 @@ struct BookDetailView: View {
                                 .fontWeight(.bold)
                         }
                         
-                        List {
+                        VStack {
+                            //viewStore.bookInfoList는 Store로 전달
                             ForEach(viewStore.bookInfoList) { item in
                                 BookDetailInnerView(title: item.category, content: item.content ?? "정보 없음")
                                 Spacer().frame(height: 10)
@@ -62,9 +64,16 @@ struct BookDetailView: View {
                     }
                     .padding()
                 }
+                .onAppear {
+                    viewStore.send(.fetchDetails(BookDetail_API.Request(isbn13: "9781491954249")))
+                }
             }
             .navigationBarHidden(true)
+            .alert(isPresented: .constant(viewStore.errorMessage != nil), content: {
+                Alert(title: Text("Error"), message: Text(viewStore.errorMessage ?? ""), dismissButton: .default(Text("OK")))
+            })
         }
         
     }
 }
+
